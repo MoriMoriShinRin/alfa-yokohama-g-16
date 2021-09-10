b53d7f613d71d2808f873805efab76b5d98202d5
MoriMoriShinRin <89884426+MoriMoriShinRin@users.noreply.github.com>
2021-09-10 10:16:13 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'とびら',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'かがみ',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'じゃんぷ',
           // q2: 'かかか',
           // q3: 'ききき',
         }
